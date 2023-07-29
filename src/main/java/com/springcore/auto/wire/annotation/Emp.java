26eaa13f6d7249328fb099633ee1eab1c79fb64f
anodit <mohitkt2247@gmail.com>
2023-07-29 15:27:49 +0530

    @Qualifier annotation example

--- src/main/java/com/springcore/auto/wire/annotation/Emp.java
+++ src/main/java/com/springcore/auto/wire/annotation/Emp.java
@@ -1,21 +1,23 @@
 package com.springcore.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
 	@Autowired
+	@Qualifier("temp2")
 	private Address address;
 
 	public Address getAddress() {
 		return address;
 	}
 
-
+	
 	public void setAddress(Address address) {
 		System.out.println("setting value");
 		this.address = address;
 	}
-
+	
 	public Emp(Address address) {
 		super();
 		this.address = address;
